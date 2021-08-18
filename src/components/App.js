9947536de3a350f45b6aafb17721b4f97f67c297
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-08-18 14:25:35 -0500

    Add bulma.io as front-end framework

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from '../containers/Home';
+import Home from '../pages/Home';
 // import Stats from '../containers/Stats';
 
 const App = () => (
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => (
+  <section className="section">
+    <div className="container">
+      <h1 className="title">
+        Hello World
+        <i className="fas fa-warehouse" />
+      </h1>
+      <p className="subtitle">
+        My first website with
+        <strong>
+          Bulma
+        </strong>
+        !
+      </p>
+    </div>
+  </section>
+);
+
+export default Home;
